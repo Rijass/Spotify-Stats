53903552477e9e1cfb01f199463176991350e5c8
Kevin <kevinwengler09@gmail.com>
2025-11-27 13:25:31 +0100

    Project structure

---
+++ src/main/java/com/spotifywrapped/spotify_wrapped_clone/SpotifyWrappedCloneApplication.java
@@ -0,0 +1,13 @@
+package com.spotifywrapped.spotify_wrapped_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyWrappedCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyWrappedCloneApplication.class, args);
+	}
+
+}
