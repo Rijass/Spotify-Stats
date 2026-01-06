a06e49562982eb49babba45b40ebbd499ce73230
Kevin <kewe1002@stud.hs-kl.de>
2026-01-06 13:15:42 +0100

    Spring-Testprofil aktiviert und GitHub Actions Workflow umbenannt:
    - `@ActiveProfiles("test")` zur Testklasse `SpotifyWrappedCloneApplicationTests` hinzugefügt.
    - Workflow-Datei von `docker.yml` zu `tests.yml` umbenannt.

--- src/test/java/com/spotifywrapped/spotify_wrapped_clone/SpotifyWrappedCloneApplicationTests.java
+++ src/test/java/com/spotifywrapped/spotify_wrapped_clone/SpotifyWrappedCloneApplicationTests.java
@@ -2,8 +2,10 @@ package com.spotifywrapped.spotify_wrapped_clone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpotifyWrappedCloneApplicationTests {
 
     @Test
